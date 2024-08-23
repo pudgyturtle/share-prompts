9d0fc5ed144df20539c7c621b3355f5ac90c8682
pudgyturtle <12068664+pudgyturtle@users.noreply.github.com>
2024-08-23 13:48:03 -0500

    Boilerplate content added

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: 'DictionAIry',
+    description: 'Discover & Share AI Prompts'
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+            <div className="gradient" />
+
+            <main className="app">
+                {children} 
+            </main>
+        </div>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout;
