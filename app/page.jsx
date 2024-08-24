694c56c88b1f943a66c7d13dfbab8f4e7547cc92
pudgyturtle <12068664+pudgyturtle@users.noreply.github.com>
2024-08-24 15:19:21 -0500

    Nav framed out with logo added

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,21 @@
+import Feed from '@components/Feed';
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover & Share
+            <br className="max-md:hidden" />
+            <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+
+        </h1>
+        <p className="desc text-center">
+            DictionAIry is a free & open prompt collection to get the most out of 
+            modern AI chatbots.
+        </p>
+
+        <Feed />
+    </section>
   )
 }
 
